20fd387110cdb374f112cf5daa0e39bdc9de86ef
Bade Iriabho <ebade@yahoo.com>
2012-03-21 11:35:46 -0500

    Added countshapes module and download image feature

--- mainMenu.js
+++ mainMenu.js
@@ -5,11 +5,18 @@
 
 imagejs.msg('mainMenu loaded'); // to notify via console and div#msg
 
+function handleDownloadImage() {
+document.getElementById('cvBase').getContext('2d').drawImage(document.getElementById('cvTop'),0,0);
+    var downloadImage = imagejs.canvas2Image('cvBase'); //Get updated base canvas
+    document.getElementById('dButton').href = downloadImage.replace("image/png");
+}
+
 (function(){
 	
 	var listOfModules={
 		'hello world':function(evt){imagejs.loadModule('http://imagejs.googlecode.com/git/helloWorld.js')},
-		'chromomarkers':function(evt){imagejs.loadModule('http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js')}		
+		'chromomarkers':function(evt){imagejs.loadModule('http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js')},		
+		'Count Shapes':function(evt){imagejs.loadModules(['http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js','http://module.imagejs.googlecode.com/git/mathbiol.countshapes.js'])}
 	}
 	
 	var menu={
@@ -29,6 +36,12 @@ imagejs.msg('mainMenu loaded'); // to notify via console and div#msg
 	cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop; // make sure the two canvas are aligned
 	// --------------
 	
-	
+	var a = document.createElement('a');
+	document.body.appendChild(a);
+	a.id = "dButton";
+	a.href="";
+	a.download="samplefilename.png";
+	a.textContent="Download Image";
+	a.addEventListener('click', handleDownloadImage, false);
 	
 })()
